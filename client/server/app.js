97a5bee58add51ad40f09a38aff6f7eb0cb8e2a5
oluwaseun <omoniyi22>
2020-07-13 02:22:36 +0100

    speak on v0.1

--- client/server/app.js
+++ client/server/app.js
@@ -1,52 +1,3 @@
-// const WebSocket = require("ws")
-// const wss = new WebSocket.Server({port: 3001})
-// const users = []
-// const broadcast = (data, ws)=>{
-//     wss.clients.forEach((client)=>{
-//         if(client.readyState === WebSocket.OPEN && client !== ws){
-//             client.send(JSON.stringify(data))
-//         }
-//     })
-// }
-// wss.on('connection', (ws)=>{
-//     let index 
-//     ws.on('messsage', (message)=>{
-//         const data = JSON.parse(message)
-//         switch (data.type) {
-//             case 'ADD_USER': {
-//             index = users.length
-//             users.push({name:data.name, id: index + 1})
-//                 ws.send(JSON.stringify({
-//                     type: 'USERS_LIST',
-//                     users
-//                 }))
-//                 broadcast({
-//                     type: 'USERS_LIST',
-//                     users
-//                 }, ws)
-//                 break
-//             }
-//             case 'ADD_MESSAGE':
-//                 broadcast({
-//                     type: 'ADD_MESSAGE',
-//                     message: data.message,
-//                     author: data.author
-//                 },ws)
-//               break
-//             default:
-//                break
-//         }
-            
-//     })   
-//     ws.on('close', ()=>{
-//         users.splice(index, 1)
-//         broadcast({
-//             type: 'USERS_LIST',
-//             users
-//         }, ws)
-//     })       
-// })    
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -56,75 +7,67 @@ const path = require('path')
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 3001 })
 
+const users = []
+
+const broadcast = (data, ws) => {
+	wss.clients.forEach((client) => {
+		if (client.readyState === WebSocket.OPEN && client !== ws ) { 
+			client.send(JSON.stringify(data))
+		}
+	})
+}
+
+wss.on('connection', (ws) => {
+	let index
+	ws.on('message', (message) => {
+		const data = JSON.parse(message)
+		switch (data.type) {
+			case 'ADD_USER': {
+				index = users.length
+				users.push({ name: data.name, id: index + 1})
+				ws.send(JSON.stringify({
+					type: 'USERS_LIST',
+					users
+				}))
+				broadcast({
+					type: 'USERS_LIST',
+					users
+				}, ws)
+				break
+			}
+			case 'ADD_MESSAGE':
+				broadcast({
+					type: 'ADD_MESSAGE',
+					message: data.message,
+					author: data.author
+				}, ws)
+				break
+			default:
+				break
+		}
+	})
+	ws.on('close', () => {
+		users.splice(index, 1)
+		broadcast({
+			type: 'USERS_LIST',
+			users
+		}, ws)
+	})
+})
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-
-// const users = []
-
-// const broadcast = (data, ws) => {
-// 	wss.clients.forEach((client) => {
-// 		if (client.readyState === WebSocket.OPEN && client !== ws ) { 
-// 			client.send(JSON.stringify(data))
-// 		}
-// 	})
-// }
-
-// wss.on('connection', (ws) => {
-// 	let index
-// 	ws.on('message', (message) => {
-// 		const data = JSON.parse(message)
-// 		switch (data.type) {
-// 			case 'ADD_USER': {
-// 				index = users.length
-// 				users.push({ name: data.name, id: index + 1})
-// 				ws.send(JSON.stringify({
-// 					type: 'USERS_LIST',
-// 					users
-// 				}))
-// 				broadcast({
-// 					type: 'USERS_LIST',
-// 					users
-// 				}, ws)
-// 				break
-// 			}
-// 			case 'ADD_MESSAGE':
-// 				broadcast({
-// 					type: 'ADD_MESSAGE',
-// 					message: data.message,
-// 					author: data.author
-// 				}, ws)
-// 				break
-// 			default:
-// 				break
-// 		}
-// 	})
-// 	ws.on('close', () => {
-// 		users.splice(index, 1)
-// 		broadcast({
-// 			type: 'USERS_LIST',
-// 			users
-// 		}, ws)
-// 	})
-// })
-
-
-
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 const build_path = path.join(__dirname, '..', 'build')
 app.use(express.static(build_path))
 app.get('/*', (req, res) => {
     res.sendFile(path.join(build_path, 'index.html'))
 })
-// }
+}
 
 
 app.listen(PORT, () => {
     console.log('server is working', PORT);
 })
-
-
-

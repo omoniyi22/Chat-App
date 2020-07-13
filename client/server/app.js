6420692ecd938522e3f48a229c6684a7b18e52ea
oluwaseun <omoniyi22>
2020-07-13 02:02:15 +0100

    speak on v0.1

--- client/server/app.js
+++ client/server/app.js
@@ -52,63 +52,65 @@ const wss = new WebSocket.Server({ port: 3001 })
 const path = require('path')
 const express = require('express')
 const app = express()
+const cors = require("cors")
+app.use(cors())
 
-const users = []
+// const users = []
 
-const broadcast = (data, ws) => {
-	wss.clients.forEach((client) => {
-		if (client.readyState === WebSocket.OPEN && client !== ws ) { 
-			client.send(JSON.stringify(data))
-		}
-	})
-}
+// const broadcast = (data, ws) => {
+// 	wss.clients.forEach((client) => {
+// 		if (client.readyState === WebSocket.OPEN && client !== ws ) { 
+// 			client.send(JSON.stringify(data))
+// 		}
+// 	})
+// }
 
-wss.on('connection', (ws) => {
-	let index
-	ws.on('message', (message) => {
-		const data = JSON.parse(message)
-		switch (data.type) {
-			case 'ADD_USER': {
-				index = users.length
-				users.push({ name: data.name, id: index + 1})
-				ws.send(JSON.stringify({
-					type: 'USERS_LIST',
-					users
-				}))
-				broadcast({
-					type: 'USERS_LIST',
-					users
-				}, ws)
-				break
-			}
-			case 'ADD_MESSAGE':
-				broadcast({
-					type: 'ADD_MESSAGE',
-					message: data.message,
-					author: data.author
-				}, ws)
-				break
-			default:
-				break
-		}
-	})
-	ws.on('close', () => {
-		users.splice(index, 1)
-		broadcast({
-			type: 'USERS_LIST',
-			users
-		}, ws)
-	})
-})
+// wss.on('connection', (ws) => {
+// 	let index
+// 	ws.on('message', (message) => {
+// 		const data = JSON.parse(message)
+// 		switch (data.type) {
+// 			case 'ADD_USER': {
+// 				index = users.length
+// 				users.push({ name: data.name, id: index + 1})
+// 				ws.send(JSON.stringify({
+// 					type: 'USERS_LIST',
+// 					users
+// 				}))
+// 				broadcast({
+// 					type: 'USERS_LIST',
+// 					users
+// 				}, ws)
+// 				break
+// 			}
+// 			case 'ADD_MESSAGE':
+// 				broadcast({
+// 					type: 'ADD_MESSAGE',
+// 					message: data.message,
+// 					author: data.author
+// 				}, ws)
+// 				break
+// 			default:
+// 				break
+// 		}
+// 	})
+// 	ws.on('close', () => {
+// 		users.splice(index, 1)
+// 		broadcast({
+// 			type: 'USERS_LIST',
+// 			users
+// 		}, ws)
+// 	})
+// })
 
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     const build_path = path.join(__dirname, '..', 'build')
     app.use(express.static(build_path))
     app.get('/*', (req, res) => {
         res.sendFile(path.join(build_path, 'index.html'))
     })
-}
+// }
 
 app.listen(3002,()=>{
 	console.log('Yes working')
